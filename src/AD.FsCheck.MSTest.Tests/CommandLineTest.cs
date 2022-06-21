33680d625e882f71e501c912d6d0a4516e054c6e
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-06-21 20:13:10 +0200

    CommandLineTest

---
+++ src/AD.FsCheck.MSTest.Tests/CommandLineTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace AD.FsCheck.MSTest.Tests;
+
+public abstract class CommandLineTest
+{
+    public const string EnvironmentVariable = "CommandLine";
+
+    static readonly Regex SuccessRegex = new(@"^Ok, passed \d+ tests.$");
+
+    private readonly string className;
+
+    public CommandLineTest(string className)
+    {
+        this.className = className;
+    }
+
+    protected async Task AssertSuccess(string testName)
+    {
+        var result = await Run(testName);
+        Assert.IsTrue(SuccessRegex.IsMatch(result));
+    }
+
+    async Task<string> Run(string testName)
+    {
+        var fileName = CreateTestFileName();
+        await RunCommandLineTest(testName, fileName);
+        var output = await FetchTestOutput(fileName);
+        File.Delete(fileName);
+        return output;
+    }
+
+    static string CreateTestFileName() => Path.ChangeExtension(Path.GetTempFileName(), ".trx");
+
+    async Task RunCommandLineTest(string testName, string fileName) =>
+        await Process.Start("dotnet.exe", $@"test ..\..\..\. --no-build --environment {EnvironmentVariable}=true --logger ""trx;LogFileName={fileName}"" --filter ""FullyQualifiedName=AD.FsCheck.MSTest.Tests.{className}.{testName}""").WaitForExitAsync();
+
+    static async Task<string> FetchTestOutput(string fileName)
+    {
+        string output;
+        using var reader = new StreamReader(fileName);
+        var result = await XDocument.LoadAsync(reader, LoadOptions.None, CancellationToken.None);
+        output = result.Descendants(XName.Get("StdOut", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010")).Single().Value;
+
+        return output;
+    }
+}
---
+++ src/AD.FsCheck.MSTest.Tests/ParametersTest.cs
@@ -0,0 +1,17 @@
+﻿namespace AD.FsCheck.MSTest.Tests;
+
+[TestClass]
+public class ParametersTest : CommandLineTest
+{
+    public ParametersTest() : base(nameof(ParametersTest))
+    { }
+
+    [CommandLine(true), Property]
+    public void One_parameter(int a)
+    {
+
+    }
+
+    [CommandLine(false), TestMethod]
+    public async Task One_parameter_test() => await AssertSuccess(nameof(One_parameter));
+}
