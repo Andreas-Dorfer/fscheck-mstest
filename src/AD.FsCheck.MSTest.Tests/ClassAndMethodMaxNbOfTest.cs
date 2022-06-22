f44a28fd6c2111b273fb30f03ec07742be5009ba
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-06-22 09:21:25 +0200

    MaxNbOfTest tests

---
+++ src/AD.FsCheck.MSTest.Tests/ClassAndMethodMaxNbOfTest.cs
@@ -0,0 +1,29 @@
+﻿namespace AD.FsCheck.MSTest.Tests;
+
+#pragma warning disable IDE0060 // Remove unused parameter
+
+[Properties(MaxNbOfTest = ClassMaxNbOfTest)]
+public class ClassAndMethodMaxNbOfTest : CommandLineTest
+{
+    const int ClassMaxNbOfTest = 20;
+    const int MethodMaxNbOfTest = 30;
+
+    public ClassAndMethodMaxNbOfTest() : base(nameof(ClassAndMethodMaxNbOfTest))
+    { }
+
+    [CommandLineProperty]
+    public void Class_is_inherited(int a)
+    { }
+
+    [TestMethod]
+    public async Task Class_is_inherited_test() => Assert.AreEqual(ClassMaxNbOfTest, await AssertSuccess(nameof(Class_is_inherited)));
+
+    [CommandLineProperty(MaxNbOfTest = MethodMaxNbOfTest)]
+    public void Method_overrides_class(int a)
+    { }
+
+    [TestMethod]
+    public async Task Method_overrides_class_test() => Assert.AreEqual(MethodMaxNbOfTest, await AssertSuccess(nameof(Method_overrides_class)));
+}
+
+#pragma warning restore IDE0060 // Remove unused parameter
