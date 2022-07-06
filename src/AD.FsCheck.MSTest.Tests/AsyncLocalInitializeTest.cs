6850cc75f2e7282d3ad83db95f8cb0685bc75870
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-07-06 22:36:53 +0200

    AsyncLocalInitializeTest

---
+++ src/AD.FsCheck.MSTest.Tests/AsyncLocalInitializeTest.cs
@@ -0,0 +1,31 @@
+﻿namespace AD.FsCheck.MSTest.Tests;
+
+[TestClass]
+public class AsyncLocalInitializeTest
+{
+    static readonly AsyncLocal<Guid> propId = new AsyncLocal<Guid>();
+
+    [PropertyInitialize]
+    public static void PropertyInitialize()
+    {
+        propId.Value = Guid.NewGuid();
+    }
+
+    [Property]
+    public async Task PropA(int a)
+    {
+        var id = propId.Value;
+        AreNotEqual(Guid.Empty, id);
+        await Task.Delay(10).ConfigureAwait(false);
+        AreEqual(id, propId.Value);
+    }
+
+    [Property]
+    public async Task PropB(int a)
+    {
+        var id = propId.Value;
+        AreNotEqual(Guid.Empty, id);
+        await Task.Delay(10).ConfigureAwait(false);
+        AreEqual(id, propId.Value);
+    }
+}
