bf0c0d600d4a36fc66ad7c17686e82d939c89350
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2022-10-17 17:45:30 +0200

    format tests

--- src/AD.FsCheck.MSTest.Tests/PropertiesReplayTest.cs
+++ src/AD.FsCheck.MSTest.Tests/PropertiesReplayTest.cs
@@ -10,7 +10,7 @@ public class PropertiesReplayTest : CommandLineTest
     public void Prop(int a) => IsTrue(a < 17);
 
     [TestMethod]
-    public async Task Propt_test()
+    public async Task Prop_test()
     {
         var msg = await Run(nameof(Prop), Fetch.StdErr);
         IsTrue(msg.StartsWith("Falsifiable, after 28 tests (1 shrink) (StdGen (1561428431,297099475))"));
--- src/AD.FsCheck.MSTest.Tests/PropertyReplayTest.cs
+++ src/AD.FsCheck.MSTest.Tests/PropertyReplayTest.cs
@@ -7,12 +7,26 @@ public class PropertyReplayTest : CommandLineTest
     { }
 
     [CommandLineProperty(Replay = "1561428431,297099475")]
-    public void Prop(int a) => IsTrue(a < 17);
+    public void Prop1(int a) => IsTrue(a < 17);
 
     [TestMethod]
-    public async Task Propt_test()
+    public async Task Prop1_test() => await Run(nameof(Prop1));
+
+    [CommandLineProperty(Replay = " 1561428431   , 297099475 ")]
+    public void Prop2(int a) => IsTrue(a < 17);
+
+    [TestMethod]
+    public async Task Prop2_test() => await Run(nameof(Prop2));
+
+    [CommandLineProperty(Replay = "(1561428431,297099475)")]
+    public void Prop3(int a) => IsTrue(a < 17);
+
+    [TestMethod]
+    public async Task Prop3_test() => await Run(nameof(Prop3));
+
+    async Task Run(string testName)
     {
-        var msg = await Run(nameof(Prop), Fetch.StdErr);
+        var msg = await Run(testName, Fetch.StdErr);
         IsTrue(msg.StartsWith("Falsifiable, after 28 tests (1 shrink) (StdGen (1561428431,297099475))"));
     }
 }
