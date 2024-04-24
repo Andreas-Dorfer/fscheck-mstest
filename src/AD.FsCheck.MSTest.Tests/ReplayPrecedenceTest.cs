e581dd6ed9a0ff97f74882e4912b28c888031d27
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2024-04-24 19:24:52 +0200

    test fix

--- src/AD.FsCheck.MSTest.Tests/ReplayPrecedenceTest.cs
+++ src/AD.FsCheck.MSTest.Tests/ReplayPrecedenceTest.cs
@@ -6,13 +6,13 @@ public sealed class ReplayPrecedenceTest : CommandLineTest
     public ReplayPrecedenceTest() : base(nameof(ReplayPrecedenceTest))
     { }
 
-    [CommandLineProperty(Replay = "1561428431,297099475")]
+    [CommandLineProperty(Replay = "5195330141687306492,9724345478383734501")]
     public void Prop(int a) => IsTrue(a < 17);
 
     [TestMethod]
     public async Task Prop_test()
     {
         var msg = await Run(nameof(Prop), Fetch.StdErr);
-        IsTrue(msg.StartsWith("Falsifiable, after 28 tests (1 shrink) (StdGen (1561428431,297099475))"));
+        IsTrue(msg.StartsWith("Falsifiable, after 21 tests (2 shrinks) (5195330141687306492,9724345478383734501)"));
     }
 }
