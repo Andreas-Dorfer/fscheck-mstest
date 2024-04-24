3fcbe9d5980ba21662486db1d66eebed9b31cf9f
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2024-04-24 19:21:55 +0200

    test fix

--- src/AD.FsCheck.MSTest.Tests/PropertiesReplayTest.cs
+++ src/AD.FsCheck.MSTest.Tests/PropertiesReplayTest.cs
@@ -1,6 +1,6 @@
 ﻿namespace AD.FsCheck.MSTest.Tests;
 
-[Properties(Replay = "1561428431,297099475")]
+[Properties(Replay = "10333802694858096737,13365262137747244545")]
 public sealed class PropertiesReplayTest : CommandLineTest
 {
     public PropertiesReplayTest() : base(nameof(PropertiesReplayTest))
@@ -13,6 +13,6 @@ public sealed class PropertiesReplayTest : CommandLineTest
     public async Task Prop_test()
     {
         var msg = await Run(nameof(Prop), Fetch.StdErr);
-        IsTrue(msg.StartsWith("Falsifiable, after 28 tests (1 shrink) (StdGen (1561428431,297099475))"));
+        IsTrue(msg.StartsWith("Falsifiable, after 20 tests (2 shrinks) (10333802694858096737,13365262137747244545)"));
     }
 }
