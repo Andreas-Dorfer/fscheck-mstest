344cb5c069fdfeaad01afec43a38778564448d58
Alan Horne <alan.horne@gmail.com>
2024-09-11 12:00:01 -0500

    Added netstandard2.0 to target frameworks. (#18)
    Co-authored-by: Alan Horne <ahorne@retrostudios.com>

---
+++ src/AD.FsCheck.MSTest/IsExternalInit.cs
@@ -0,0 +1,10 @@
+// from https://github.com/dotnet/runtime/blob/48ae96799fa048302d5e323cee204ae7fbb4edde/src/tasks/Common/IsExternalInit.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+   internal sealed class IsExternalInit { }
+}
