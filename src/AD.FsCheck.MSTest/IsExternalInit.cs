31d2ac967d72466bc31a7d4a5fb6c04930c7f6e8
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2026-01-14 18:33:50 +0100

    fix

--- src/AD.FsCheck.MSTest/IsExternalInit.cs
+++ src/AD.FsCheck.MSTest/IsExternalInit.cs
@@ -8,4 +8,4 @@
 namespace System.Runtime.CompilerServices;
 #pragma warning restore IDE0130 // Namespace does not match folder structure
 
-internal static class IsExternalInit;
+internal sealed class IsExternalInit;
